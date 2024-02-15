2d9278bf45a045bfa398f403f84a1edc44062e18
rockgem <63050183+rockgem@users.noreply.github.com>
2024-02-15 20:16:18 +0800

    open/close elevetor

--- actors/objects/elevator switch.gd
+++ actors/objects/elevator switch.gd
@@ -1,10 +1,23 @@
 extends Area2D
 
+signal elevator_press
+
+@onready var sprite = $AnimatedSprite2D
+
 @export var link_code: int = 0
-signal elevator_press()
 
-#const TEXTURES: Dictionary = {
-	#'called': preload("res://astronaught/interior_assets/elevator_called.png"),
-	#'stopped': ''
-#}
 
+var is_open = false
+
+
+
+func _on_input_event(viewport, event, shape_idx):
+	if event is InputEventScreenTouch and !event.pressed:
+		if is_open == false:
+			sprite.play("open")
+		else:
+			sprite.play("close")
+		
+		is_open = !is_open
+		
+		elevator_press.emit()
