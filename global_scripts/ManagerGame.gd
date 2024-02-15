a907d749ade8c99f7d6d53886ca42f47d5e98d80
rockgem <63050183+rockgem@users.noreply.github.com>
2024-02-15 20:32:15 +0800

    create global game manager singleton

---
+++ global_scripts/ManagerGame.gd
@@ -0,0 +1,4 @@
+extends Node
+
+
+var global_player_ui_ref = null
--- scenes/UI.gd
+++ scenes/UI.gd
@@ -2,6 +2,10 @@ extends Control
 
 
 
+func _ready():
+	ManagerGame.global_player_ui_ref = self
+
+
 func pop_to_ui(instance):
 	for child in $Popups.get_children():
 		child.queue_free()
