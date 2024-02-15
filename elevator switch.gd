43c355089a4d16e3844c018b967a8788098708e5
rockgem <63050183+rockgem@users.noreply.github.com>
2024-02-15 19:05:23 +0800

    fix scripts

--- elevator switch.gd
+++ elevator switch.gd
@@ -1,10 +1,10 @@
 extends Area2D
 
-export(int) var link_code: int = 0
+@export var link_code: int = 0
 signal elevator_press()
 
-const TEXTURES Dictionary = {
-	'called': preload("res://astronaught/interior_assets/elevator_called.png")
-	'stopped': 
-}
+#const TEXTURES: Dictionary = {
+	#'called': preload("res://astronaught/interior_assets/elevator_called.png"),
+	#'stopped': ''
+#}
 
